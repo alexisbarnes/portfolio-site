08ed45368038d84c30200e085bfe320e6c826926
alexisbarnes <barnesan@live.unc.edu>
2016-05-09 20:52:51 -0400

    making progress

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   console.log("DOC Ready!");
 
+  $(window).load(function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+  });
 
-})
-
-
-//mobile menu
+});
